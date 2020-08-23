439ad881a9c2456ee94817361ebdae47cf9719ab
Michael Oldacre <mloldacre@live.com>
2020-08-23 14:16:12 -0400

    Sanitizing done

--- server.js
+++ server.js
@@ -49,6 +49,19 @@ function handleGetPokemon(req, res) {
 }
 app.get('/pokemon', handleGetPokemon);
 
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+
+//server
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
